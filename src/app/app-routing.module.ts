dfa887094e9d40b7e45a216ecf5174148cb357cd
Tanmay Sarin <tanmaysarin@gmail.com>
2023-04-13 01:30:14 -0500

    created main page view, added heading and columns html along with their scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainViewComponent } from './pages/main-view/main-view.component';
 
-const routes: Routes = [];
+// Helps in finalizing the view to be displayed when we go to the base url (in current case: localhost:4200)
+
+const routes: Routes = [
+  { path: '', component: MainViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
